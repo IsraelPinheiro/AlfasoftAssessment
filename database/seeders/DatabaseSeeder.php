23f8cebaaaff87df45855e7047aca98273eb6a6a
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 13:01:39 -0300

    Set defauld dev env to local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder{
     /**
      * Seed the application's database.
      *
@@ -14,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(){
         $this->call(ProfileSeeder::class);
         $this->call(UserSeeder::class);
-        //Seeders to be run only on dev environment
-        if(app()->environment('dev')){
+        //Seeders to be run only on local environment
+        if(app()->environment('local')){
             $this->call(ContactSeeder::class);
         }
         //Seeders to be run only on production environment
