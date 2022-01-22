7c028e12fac515d15a8bfc3ff25d200fcd5d136d
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 11:50:12 -0300

    Add Footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            Copyright &copy;{{ Carbon\Carbon::now()->year;}} | Made by <a href="https://github.com/IsraelPinheiro" class="font-weight-bold">Israel Pinheiro</a> with <i class='fa fa-heart pulse' style="color:#dc3545"></i> for <a href="https://www.alfasoft.pt" class="font-weight-bold">Alfasoft</a>.
+        </div>
+    </div>
+</footer>
