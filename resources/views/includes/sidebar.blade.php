7b89fcb472b6fcaa610c6e74483ca39cd5701df7
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 17:14:16 -0300

    Change icon

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
     {{-- Sidebar - Brand --}}
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-archive"></i>
+            <i class="fas fa-phone"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Contacts</div>
     </a>
