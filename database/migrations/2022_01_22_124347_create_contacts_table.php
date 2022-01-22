9c6da2b4758015fe39d783cb48a8df01d7b80c4e
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 10:14:50 -0300

    Add contacts table base structure to the boilerplate file

--- database/migrations/2022_01_22_124347_create_contacts_table.php
+++ database/migrations/2022_01_22_124347_create_contacts_table.php
@@ -4,18 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactsTable extends Migration
-{
+class CreateContactsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('contacts', function (Blueprint $table) {
+    public function up(){
+        Schema::create('contacts', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('contact',9)->unique();
+            $table->string('email')->unique();
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +28,7 @@ class CreateContactsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('contacts');
     }
 }
