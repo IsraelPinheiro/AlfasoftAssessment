2707c8b5159003ebb157d1fc02ab81f583548545
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 15:05:16 -0300

    Fix duplicated id

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -26,7 +26,7 @@
             <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                 <form class="form-inline mr-auto w-100 navbar-search">
                     <div class="input-group">
-                        <input id="Search" type="text" class="form-control bg-light border-0 small" placeholder="{{__("Search")}}..." aria-label="{{__("Search")}}" aria-describedby="basic-addon2">
+                        <input id="SearchXS" type="text" class="form-control bg-light border-0 small" placeholder="{{__("Search")}}..." aria-label="{{__("Search")}}" aria-describedby="basic-addon2">
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="button">
                                 <i class="fas fa-search fa-sm"></i>
