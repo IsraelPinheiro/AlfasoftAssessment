77c4c976d85339f89d8457b981f6fcbdb9e378d9
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 10:14:59 -0300

    Update users table base structure from the boilerplate file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,23 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+class CreateUsersTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up(){
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('active')->default(true);
             $table->string('password');
+            //User permissions
+            $table->boolean('can_create')->default(false);  //Flag that controls if the user can create a new entry
+            $table->boolean('can_update')->default(false);  //Flag that controls if the user can update an existing entry
+            $table->boolean('can_delete')->default(false);  //Flag that controls if the user can delete an existing entry
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -29,8 +35,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('users');
     }
 }
