d5a516f669e81dc84845c31dad9f253a7642ca94
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 11:05:02 -0300

    Add profile_id field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration{
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('profile_id')->unsigned();
             $table->boolean('active')->default(true);
             $table->string('password');
             $table->rememberToken();
