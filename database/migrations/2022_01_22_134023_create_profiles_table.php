de746e2330f42f9360dfef0b6a2e5d9654471c27
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 10:47:47 -0300

    Add profiles table base structure

--- database/migrations/2022_01_22_134023_create_profiles_table.php
+++ database/migrations/2022_01_22_134023_create_profiles_table.php
@@ -4,18 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProfilesTable extends Migration
-{
+class CreateProfilesTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('profiles', function (Blueprint $table) {
+    public function up(){
+        Schema::create('profiles', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->text('description')->nullable()->default(null);
+            //Access Controll
+            //User 
+            $table->boolean('users_create')->default(false);    //Flag that controls if the user can create a new user entry
+            $table->boolean('users_read')->default(false);      //Flag that controls if the user can access the users index page
+            $table->boolean('users_update')->default(false);    //Flag that controls if the user can update an existing user entry
+            $table->boolean('users_delete')->default(false);    //Flag that controls if the user can delete an existing user entry
+            //Contacts
+            $table->boolean('users_create')->default(false);    //Flag that controls if the user can create a new user entry
+            $table->boolean('users_update')->default(false);    //Flag that controls if the user can update an existing user entry
+            $table->boolean('users_delete')->default(false);    //Flag that controls if the user can delete an existing user entry
+            //Controll
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +38,7 @@ class CreateProfilesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('profiles');
     }
 }
