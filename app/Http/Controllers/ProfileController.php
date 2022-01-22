0e38bd592a2da45bc29a82b2bb9351a319d3613a
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-22 15:52:25 -0300

    Add Create, Store and Delete logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -28,9 +29,13 @@ class ProfileController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create(){
+        if(Auth::user()->profile->profiles_create){
+            return view('pages.profiles.new');
+        }
+        else{
+            abort(401);
+        }
     }
 
     /**
@@ -39,9 +44,32 @@ class ProfileController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request){
+        if(Auth::user()->profile->profiles_create){
+            $request->validate([
+                'Name' => 'bail|required|min:3|string|unique:profiles',
+            ]);
+            $profile = new Profile;
+            $profile->name = $request->Name;
+            $profile->description = $request->Description;
+            $profile->contacts_create = $request->contacts_create ? 1:0;
+            $profile->contacts_update = $request->contacts_update ? 1:0;
+            $profile->contacts_delete = $request->contacts_delete ? 1:0;
+            $profile->users_create = $request->users_create ? 1:0;
+            $profile->users_read = $request->users_read ? 1:0;
+            $profile->users_update = $request->users_update ? 1:0;
+            $profile->users_delete = $request->users_delete ? 1:0;
+            $profile->profiles_create = $request->profiles_create ? 1:0;
+            $profile->profiles_read = $request->profiles_read ? 1:0;
+            $profile->profiles_update = $request->profiles_update ? 1:0;
+            $profile->profiles_delete = $request->profiles_delete ? 1:0;
+            $profile->created_by = Auth::user()->id;
+            $profile->save();
+            return response()->json(['message' => __('Profile Created')],200);
+        }
+        else{
+            abort(401);
+        }
     }
 
     /**
@@ -84,8 +112,19 @@ class ProfileController extends Controller
      * @param  \App\Models\Profile  $profile
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Profile $profile)
-    {
-        //
+    public function destroy(Profile $profile){
+        if(Auth::user()->profile->profiles_delete){
+            $profile = Profile::find($profile->id);
+            if($profile){
+                $profile->delete();
+                return response()->json(['level' => 'success','message' => 'Profile Deleted'],200);
+            }
+            else{
+                return response()->json(['message' => 'Profile not found'],404);
+            }
+        }
+        else{
+            abort(401);
+        }
     }
 }
